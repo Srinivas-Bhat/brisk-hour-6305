f1f487591d49be0520dda9fafdb0ee433aabfa42
Srinivas-Bhat <srinivaskbhat840@gmail.com>
2022-07-24 05:37:21 +0530

    blue mercury clone day-5

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import AllRoutes from './Components/AllRoutes';
 import Login from './Components/Login';
 import Navbar from './Components/Navbar';
+import Signup from './Components/Signup';
 import Mens from './Pages/Mens';
 import ProductDetails from './Pages/ProductDetails';
 
@@ -10,11 +11,12 @@ import ProductDetails from './Pages/ProductDetails';
 function App() {
   return (
     <div className="App">
-      <AllRoutes/>
+      {/* <AllRoutes/> */}
       {/* <Mens/> */}
       {/* <Navbar/> */}
       {/* <ProductDetails/> */}
       {/* <Login/> */}
+      <Signup/>
     </div>
   );
 }
--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,8 +1,52 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import "../Styles/Signup.css"
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div>
+        <div id="container">
+            <div id="create_container">
+                <header id="heading">
+                    <h1 id="create_account">CREATE ACCOUNT</h1>
+                    <p id="complete_form">Complete the form below to create a bluemercury.com account.</p>
+                </header>
+                <form id="form" onsubmit="store(event)">
+                    <input type="email" id="email" class="input" placeholder="Email*" required />
+                    <input type="text"  id="first_name" class="input" placeholder="First Name*" required/>
+                    <input type="text" id="last_name" class="input" placeholder="Last Name*" required/>
+                    <input type="password" id="password" class="input" placeholder="Password*" required/>
+                    <input type="date" id="birthday" class="input" placeholder="Birthday*" required/>
+                    <p id="input_required">*Indicates Required Field</p>
+                
+                <div id="checkbox_join_container">
+                    <div id="flex1">
+                        <input type="checkbox" name="" id="input"/>
+                    </div>
+                    <div id="flex2">
+                        <p id="join">join</p>
+                        <p id="bluerewards">BLUEREWARDS</p>
+                    </div>
+                    <div id="flex3">
+                        <p id="offers_signup">Earn $10 for every $250px spend, plus FREE birthday gifts & treatments!</p>
+                    </div>
+                </div>
+                <div>
+                    <p id="privacy_policy">To learn more about how we use your information and your rights, see <a href="Signup.html">Privacy Note</a></p>
+                </div>
+                <button id="create_account_btn" type="submit" >CREATE ACCOUNT</button>
+
+                <div>
+                    <span id="already_account">
+                        Already have a bluemercury.com account?
+                        <Link id="sign_in" to="/login">Sign In</Link>
+                    </span>
+                </div>
+            </form>
+            </div>        
+        </div>        
+    </div>
+
   )
 }
 
